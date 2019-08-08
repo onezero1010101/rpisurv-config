132686949bbc8c13123b370e127011d47017373a
onezero1010101 <onezero1010101@gmail.com>
2019-08-08 09:52:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # update apt
 sudo apt update
 
+# upgrade pi
+sudo apt upgrade -y
+
 # install git
 sudo apt install git -y
 
@@ -12,10 +15,12 @@ git clone https://github.com/SvenVD/rpisurv
 
 # change gpu_mem=512 in your /boot/config.txt
 #sudo sed -i 's/gpu_mem=??/gpu_mem=512/g' /boot/config.txt
-sudo echo "gpu_mem=512" >> /boot/config.txt
+echo "gpu_mem=512" | sudo tee -a /boot/config.txt
 
 # disable overscan
 sudo sed -i 's/#disable_overscan=1/disable_overscan=1/g' /boot/config.txt
+
 # rpisurv install
-sudo ./install.sh
+cd ~/rpisurv
+echo -ne '\n' | sudo ./install.sh
 
