1e1d6f113cd9729c13ccd823e84dd51a558a9850
onezero1010101 <onezero1010101@gmail.com>
2019-08-08 12:02:44 -0400

    Create rpiconfig.sh

---
+++ rpiconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/onezero1010101/rpisurv-config/master/rpisurv-config.sh -O ~/rpisurv/rpisurv-config.sh
+chmod +x ~/rpisurv/rpisurv-config.sh
+
+# install is done
+echo "Install is complete!"
+echo "Please run ~/rpisurv/rpisurv-config.sh"
+echo "to complete install and reboot."
