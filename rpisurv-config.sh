204721dc41af46a480f342bed6009dab8540fd06
onezero1010101 <onezero1010101@gmail.com>
2019-08-08 12:10:53 -0400

    Update rpisurv-config.sh

--- rpisurv-config.sh
+++ rpisurv-config.sh
@@ -7,15 +7,18 @@ do
     case $opt in
         "Animal")
             echo "AnimalSecurityPi" | sudo tee /etc/hostname
+            echo "127.0.0.1  AnimalSecurityPi" | sudo tee /etc/hosts
             sudo wget https://raw.githubusercontent.com/onezero1010101/rpisurv-config/master/rpisurv.conf.animal -O /etc/rpisurv.conf
             ;;
         "April")
             echo "AprilSecurityPi" | sudo tee /etc/hostname
+            echo "127.0.0.1  AprilSecurityPi" | sudo tee /etc/hosts
             sudo wget https://raw.githubusercontent.com/onezero1010101/rpisurv-config/master/rpisurv.conf.april -O /etc/rpisurv.conf
             break
             ;;
         "Stacie")
             echo "StacieSecurityPi" | sudo tee /etc/hostname
+            echo "127.0.0.1  StacieSecurityPi" | sudo tee /etc/hosts
 sudo wget https://raw.githubusercontent.com/onezero1010101/rpisurv-config/master/rpisurv.conf.stacie -O /etc/rpisurv.conf
             ;;
         "Quit")
